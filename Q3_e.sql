a4a1f78d9e599740d70e6d176518298e34165fa1
Nour El Natour <nour_elnatour@hotmail.com>
2020-02-26 22:46:30 -0500

    Modification for part e

--- Q3_e.sql
+++ Q3_e.sql
@@ -7,4 +7,5 @@
 select distinct m.title
 from movies m, tags t1, tags t2, tag_names tn1, tag_names tn2
 where m.mid = t1.mid and t1.tid = tn1.tid and tn1.tag like '%good%'
-and m.mid = t2.mid and t2.tid = tn2.tid and tn2.tag like '%bad%';
+and m.mid = t2.mid and t2.tid = tn2.tid and tn2.tag like '%bad%'
+and tn1.tag <> tn2.tag;
